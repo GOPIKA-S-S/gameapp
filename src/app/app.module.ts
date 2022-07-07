3c30329c14d14c62a024436b78ddb86ec437aeb1
GOPIKA-S-S <gopikass1024@gmail.com>
2022-07-07 06:18:07 +0530

    viewgame completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
+import { ViewgameComponent } from './viewgame/viewgame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontpageComponent
+    FrontpageComponent,
+    ViewgameComponent
   ],
   imports: [
     BrowserModule,
